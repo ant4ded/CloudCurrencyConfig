f380d2f4fe6bc02a654d00f687ae088cab409e41
adzedzik <adzedzik@mdait.by>
2026-03-22 17:53:14 +0300

    add missed annotation

--- src/main/java/my/idp/spring/cloudcurrencyconfig/CloudCurrencyConfigApplication.java
+++ src/main/java/my/idp/spring/cloudcurrencyconfig/CloudCurrencyConfigApplication.java
@@ -2,8 +2,10 @@ package my.idp.spring.cloudcurrencyconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloudCurrencyConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudCurrencyConfigApplication.class, args);
