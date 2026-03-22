12b2c672631585f4a8c411ed50759c8002b6fc72
adzedzik <adzedzik@mdait.by>
2026-03-22 17:43:22 +0300

    Initial commit

---
+++ src/main/java/my/idp/spring/cloudcurrencyconfig/CloudCurrencyConfigApplication.java
@@ -0,0 +1,11 @@
+package my.idp.spring.cloudcurrencyconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCurrencyConfigApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCurrencyConfigApplication.class, args);
+	}
+}
